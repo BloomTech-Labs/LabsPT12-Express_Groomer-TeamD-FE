ef4573af7d75bdcb85e818b818909a8fa496ed97
candaceyw <candaceyw@gmail.com>
2020-11-02 14:25:56 -0500

    updated some styling and updated navigation to hamburger menu for mobile

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -6,6 +6,7 @@ import { LoginPage } from '../pages/Login';
 
 const NavBar = () => {
   const [visible, setVisible] = useState(false);
+  const [open, setOpen] = useState(true);
 
   const showModal = () => {
     setVisible(true);
@@ -27,6 +28,7 @@ const NavBar = () => {
     <div className="nav-container">
       <img src={Logo} alt="groom-logo" className="logo" />
       <div className="nav-links">
+        <a href="/search"> Search for Groomers</a>
         <a href="#about">About</a>
         <a href="#testimonial">Testimonial</a>
         <p onClick={showModal}>Login</p>
@@ -35,6 +37,27 @@ const NavBar = () => {
       <Modal visible={visible} onOk={handleOk} onCancel={handleCancel}>
         <LoginPage />
       </Modal>
+
+      {/* HAMBURGER MENUE */}
+
+      <div className="menu-button" onClick={() => setOpen(!open)}>
+        <i className="fas fa-bars"></i>
+      </div>
+      {/* Sidebar */}
+
+      <nav className={`nav ${open ? 'hide' : 'show'}`}>
+        <ul className="menu-items">
+          <a href="#about" className="menu-link">
+            About
+          </a>
+          <a href="#testimonial" className="menu-link">
+            Testimonial
+          </a>
+          <p onClick={showModal} className="menu-link">
+            Login
+          </p>
+        </ul>
+      </nav>
     </div>
   );
 };
