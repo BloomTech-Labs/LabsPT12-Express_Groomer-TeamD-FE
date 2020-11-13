6ec513f81dccd984d81f07a89d9fd10017b3e3c5
candacewilson <candaceyw@gmail.com>
2020-11-12 22:42:36 -0500

    added custom markers to the map that are clickable and render a popup of the groomer profile information

---
+++ src/components/Nav/SecondaryNav.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import './nav-styles.scss';
+import Logo from '../../assets/logo.png';
+import { Modal } from 'antd';
+import { LoginPage } from '../pages/Login';
+
+const SecNav = () => {
+  const [visible, setVisible] = useState(false);
+  const [open, setOpen] = useState(true);
+
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleOk = e => {
+    console.log(e);
+    setVisible(false);
+  };
+
+  const handleCancel = e => {
+    console.log(e);
+    setVisible({
+      visible: false,
+    });
+  };
+
+  return (
+    <div className="nav-container">
+      <a href="/">
+        <img src={Logo} alt="groom-logo" className="logo" />
+      </a>
+      <div className="nav-links">
+        <a href="/search"> Search for Groomers</a>
+        <a href="/#about">About</a>
+        <a href="/#testimonial">Testimonial</a>
+        <p onClick={showModal}>Login</p>
+      </div>
+
+      <Modal visible={visible} onOk={handleOk} onCancel={handleCancel}>
+        <LoginPage />
+      </Modal>
+
+      {/* HAMBURGER MENUE */}
+
+      <div className="menu-button" onClick={() => setOpen(!open)}>
+        <i className="fas fa-bars"></i>
+      </div>
+      {/* Sidebar */}
+
+      <nav className={`nav ${open ? 'hide' : 'show'}`}>
+        <ul className="menu-items">
+          <a href="/#about" className="menu-link">
+            About
+          </a>
+          <a href="/#testimonial" className="menu-link">
+            Testimonial
+          </a>
+          <p onClick={showModal} className="menu-link">
+            Login
+          </p>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default SecNav;
